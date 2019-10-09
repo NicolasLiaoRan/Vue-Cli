4410d1b8c2a3aafd90445be5a1f5c86b8bb0c49a
NicolasLiaoRan <329512247@qq.com>
2019-10-09 15:29:48 +0800

    1

---
+++ src/views/Info.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>hello from info component</h1>
+    <button type="button" @click="add()">添加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'info',
+  methods:{
+    add(){
+      console.log("add event")
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
